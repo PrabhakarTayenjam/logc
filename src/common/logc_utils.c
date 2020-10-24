8be581f783b26e947eb9bd2513006f48de91b844
PrabhakarTayenjam <ptayenjam1995@gmail.com>
2020-10-24 15:07:07 +0530

    logc utils

---
+++ src/common/logc_utils.c
@@ -0,0 +1,68 @@
+/**
+ * MIT License
+ * 
+ * Copyright (c) 2020 Stardust
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "logc_utils.h"
+
+#include <sys/mman.h>
+#include <sys/shm.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+#ifdef LOGC_DEBUG
+void
+console_log__(char *file, char *function, int line, char *format, ...)
+{
+    char log_buff[512];
+    int n = 0;
+
+    n += sprintf(log_buff, "%s | %s | %d | ", file, function, line);
+
+    va_list va_args;
+    va_start(va_args, format);
+    vsnprintf(log_buff + n, 512 - n, format, va_args);
+
+    printf("%s\n", log_buff);
+}
+#endif
+
+void *
+create_shared_mem(char *name)
+{
+    int fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+    if(fd == -1) {
+        return NULL;
+    }
+
+    ftruncate(fd, MAX_LOG_BUFF_SIZE);
+
+    void *addr = mmap(NULL, MAX_LOG_BUFF_SIZE, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
+    if(addr == MAP_FAILED) {
+        return NULL;
+    }
+
+    close(fd);
+    return addr;
+}
---
+++ src/common/logc_utils.h
@@ -0,0 +1,57 @@
+/**
+ * MIT License
+ * 
+ * Copyright (c) 2020 Stardust
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef LOGC_UTILS_H
+#define LOGC_UTILS_H
+
+#define MAX_LOG_BUFF_SIZE     1024 * 16
+#define MAX_READ_BUFF_SIZE    128
+#define MAX_WRITE_BUFF_SIZE   128
+#define MAX_FILE_PATH_SIZE    128
+
+// Request codes
+#define REQUEST_INIT            1
+#define REQUEST_WRITE           2
+#define REQUEST_CLOSE           3
+
+
+#ifdef LOGC_DEBUG
+    void console_log__(char *file, char *function, int line, char *format, ...);
+
+    #define console_log(...) console_log__(__FILE__, (char *)__func__, __LINE__, __VA_ARGS__);
+#else
+    #define console_log(...)
+#endif
+
+
+/**
+ * Create a shared memory with the given name in read write mode.
+ * And map the file to memory.
+ * 
+ * @param name name of the shared memory file
+ * @return address of mapped memory, NULL if failed
+ */
+void *create_shared_mem(char *name);
+
+#endif
