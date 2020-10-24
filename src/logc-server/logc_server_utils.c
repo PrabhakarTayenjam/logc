416c4e58d95b63ad4478f6f7e102e2aa6f22fc95
PrabhakarTayenjam <ptayenjam1995@gmail.com>
2020-10-24 15:37:46 +0530

    server utils

---
+++ src/logc-server/logc_server_utils.c
@@ -0,0 +1,118 @@
+/**
+ * MIT License
+ * 
+ * Copyright (c) 2020 Stardust
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "logc_server.h"
+#include "logc_server_utils.h"
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/epoll.h>
+#include <sys/mman.h>
+
+
+#ifdef SERVER_LOG_ENABLE
+void server_log(char *file, int line, char *func, char *format, ...)
+{
+    char log_buff[1024];
+    int n = sprintf(log_buff, "%s | %d | %s | ", file, line, func);
+
+    va_list va_args;
+    va_start(va_args, format);
+    n += vsprintf(log_buff + n, format, va_args);
+    
+    n += sprintf(log_buff + n, "\n");
+
+    write(server_log_fd, log_buff, n);
+}
+#endif
+
+int
+accept_conn_and_add_to_epoll(int listen_fd)
+{
+    struct sockaddr_un client_addr;
+    
+    int client_fd;
+    unsigned int len;
+    client_fd = accept(listen_fd, (struct sockaddr *)(&client_addr), &len);
+    if(client_fd == -1) {
+        logc_server_log("Cannot accept connection: %s", strerror(errno));
+        return -1;
+    }
+    logc_server_log("Accepted connection from client. fd: %d", client_fd);
+
+    struct epoll_event ev;
+    ev.events = EPOLLIN;
+    ev.data.fd = client_fd;
+    int ret = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &ev);
+    if(ret == -1) {
+        logc_server_log("Cannot add to epoll: %s", strerror(errno));
+        return -1;
+    }
+    logc_server_log("Added to epoll. client fd: %d", client_fd);
+    
+    return 0;
+}
+
+void close_client(int fd)
+{
+    /**
+     * close and remove from epoll
+     * closing a fd automatically removes the fd from epoll (if all the fd references are closed)
+     */
+    close(fd);
+    // epoll_ctl(epoll_fd, EPOLL_CTL_DEL, fd, NULL);
+
+    // Get client details from  client info hash table
+    struct logc_client_info *handle;
+    handle = g_hash_table_lookup(client_info_ht, GINT_TO_POINTER(fd));
+    if(handle == NULL) {
+        logc_server_log("Client info not found. client_fd: %d", fd);
+        return;
+    }
+
+    // Read logs if there is any
+    char read_buff[MAX_LOG_BUFF_SIZE];
+    int n = logc_buffer_read_all(handle->log_buff, read_buff);
+
+    // write to file
+    if(n > 0)
+        fprintf(handle->fp, "%s", read_buff);
+
+    // Flush the log messages in buffer and close the fp
+    fflush(handle->fp);
+    fclose(handle->fp);
+
+    // unmap memory
+    munmap(handle->mmap_addr, MAX_LOG_BUFF_SIZE);
+
+    logc_server_log("Client closed. fd = %d, log_file_path: %s", fd, handle->log_file_path);
+
+    // remove from hash table. This will free client info for fd
+    g_hash_table_remove(client_info_ht, GINT_TO_POINTER(fd));
+}
---
+++ src/logc-server/logc_server_utils.h
@@ -0,0 +1,68 @@
+/**
+ * MIT License
+ * 
+ * Copyright (c) 2020 Stardust
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef LOGC_SERVER_UTILS
+#define LOGC_SERVER_UTILS
+
+#include <stdio.h>
+
+#define SERVER_LOG_ENABLE 1
+
+
+#ifdef SERVER_LOG_ENABLE
+
+void server_log(char *file, int line, char *function, char *format, ...);
+
+#define logc_server_log(...) \
+    server_log(__FILE__, __LINE__, (char *)__func__, __VA_ARGS__)
+
+#define exit_with_errno() \
+{ \
+    logc_server_log("Logc server error: %s\n", strerror(errno)); \
+    exit(1); \
+}
+
+#else
+#define logc_server_log(...)
+#endif
+
+/**
+ * Accept the connection from listen_fd and add the fd to epoll_fd
+ * 
+ * @param epoll_fd fd for the epoll interface
+ * @param listen_fd Logc server listen fd
+ * @return -1 if failed, 0 is success
+ **/
+int accept_conn_and_add_to_epoll(int listen_fd);
+
+/**
+ * Close the connection with the client 
+ * Write the log messages in the buffer to the log file
+ * Cleanup its realated informations
+ * 
+ * @param fd File descriptor of the client
+ */
+void close_client(int fd);
+
+#endif
